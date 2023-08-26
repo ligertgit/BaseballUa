494d6a2e04c7e2eaf46a6ec6b84e59ab1d8ae3ce
Kyryl Kozhushko <liger@gmail.com>
2023-08-26 18:37:41 +0300

    main events index page no next/prev button main event info page

---
+++ Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using BaseballUa.BlData;
+using BaseballUa.Data;
+using BaseballUa.DTO;
+using BaseballUa.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseballUa.Controllers
+{
+    public class EventController : Controller
+    {
+        private BaseballUaDbContext _db;
+
+        public EventController(BaseballUaDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var eventsDAL = new EventsCrud(_db).GetAll().ToList();
+            var eventsView = new EventToView().ConvertAll(eventsDAL, _db);
+            return View(eventsView);
+        }
+
+        public IActionResult DetailsEvent(int id)
+        {
+            var eventDAL = new EventsCrud(_db).Get(id);
+            var eventView = new EventToView().Convert(eventDAL, _db);
+
+            return View(eventView);
+        }
+    }
+}
